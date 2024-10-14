3cb623576c9618bb77a8254dbb413a4bdd965df1
MuKonqi <mukonqi@gmail.com>
2024-10-14 22:09:33 +0300

    Update an asset

--- script.sh
+++ script.sh
@@ -2,8 +2,9 @@
 # "I did it to make the testing of Nottodbox even easier using DistroSea."
 
 setxkbmap tr
+sudo apt update
 sudo apt install git pip ninja-build meson libxcb-cursor0 -y
-pip install PyQt6 --break-system-packages
+pip install PySide6
 git clone https://github.com/mukonqi/nottodbox.git
 cd nottodbox
 meson setup . builddir
