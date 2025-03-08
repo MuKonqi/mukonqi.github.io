c0ce886d10ee84956354a78d620bb1f0c3547437
MuKonqi <mukonqi@gmail.com>
2025-03-08 15:36:04 +0300

    Update an asset

--- script.sh
+++ script.sh
@@ -4,12 +4,7 @@
 setxkbmap tr
 sudo apt update
 sudo apt install git pip ninja-build meson libxcb-cursor0 gettext -y
-pip install PySide6
 pip install PySide6 --break-system-packages
 git clone https://github.com/mukonqi/nottodbox.git
-cd nottodbox
-meson setup . builddir
-ninja -C builddir
-sudo ninja -C builddir install
-cd ..
-rm -rf nottodbox
+meson setup nottodbox/builddir nottodbox
+meson install -C nottodbox/builddir
