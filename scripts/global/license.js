46e0a95159bceee05837803e96a8762c3510a310
MuKonqi <mukonqi@gmail.com>
2025-03-09 12:48:09 +0300

    Minor fixes (12.4.11)

--- scripts/global/license.js
+++ scripts/global/license.js
@@ -1,9 +1,19 @@
+var year = new Date().getFullYear();
+
+if (year < 2025) {
+    year = 2025;
+}
+
+document.getElementById("copyright").textContent = year;
+
+
 $.ajax({
     url: "/LICENSE.txt",
     dataType: "text",
     success: function(data) {
         document.getElementById('license').textContent = data;
     }});
+
 
 function show_license() {
     if (document.getElementById('license').style.display === "none") {
--- scripts/personal/age.js
+++ scripts/personal/age.js
@@ -4,7 +4,11 @@ const birth = new Date("2008-12-04");
 
 const today = new Date();
 
-const difference = Math.round((today.getTime() - birth.getTime()) / 1000 / (60 * 60 * 24) / 365.25);
+var difference = Math.round((today.getTime() - birth.getTime()) / 1000 / (60 * 60 * 24) / 365.25);
+
+if (difference < 16) {
+    difference = 16;
+}
 
 if (language === "english") {
     document.getElementById("age").textContent = `I finished my ${difference}th year, I'm finishing my ${difference + 1}th year in the world at the moment.`;
